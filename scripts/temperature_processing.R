fa76a2c7d61cad595c6bad57ee29d14a817ba394
mikelitzow <mike.litzow@noaa.gov>
2022-01-13 14:05:50 -0900

    Fixed bad bay/site names

--- scripts/temperature_processing.R
+++ scripts/temperature_processing.R
@@ -125,5 +125,131 @@ names(temp.dat); names(add.dat)
 
 new.dat <- rbind(temp.dat, add.dat)
 
+# check
+check.new <- new.dat %>%
+  dplyr::group_by(bay, year) %>%
+  dplyr::summarise(n = n())
+View(check.new)
+
+# need 2021 WGOA data!
+wgoa.new <- read.csv("./data/site_2021.csv")
+
+head(wgoa.new)
+
+# calculate Julian day
+wgoa$Date <- dates(as.character(wgoa$Date))
+wgoa.new$julian <- lubridate::yday(
+  lubridate::parse_date_time(x = paste(wgoa.new$year, wgoa.new$month, wgoa.new$day),
+                             orders="ymd", tz="America/Anchorage"))
+
+
+wgoa.new <- wgoa.new %>%
+  select(year, julian, Site, Bay, Temp.C)
+
+names(wgoa.new)[3:5] <- c("site", "bay", "temperature")
+
+names(new.dat)
+
+# combine!
+new.dat <- rbind(new.dat, wgoa.new)
+
+
+# check for different bay/site names
+unique(new.dat$bay)
+
+# remove Sanak I. bays - only sampled in 2021
+new.dat <- new.dat %>%
+  dplyr::filter(bay != "Caton Harbor") %>%
+  dplyr::filter(bay != "NE Harbor")
+
+check.bays <- new.dat %>%
+  dplyr::group_by(bay) %>%
+  dplyr::summarise(n = n())
+
+check.bays
+
+# remove "Bay" from Kaiugnak and Japanese, "Rodmans" to "Rodman"
+
+change <- new.dat$bay == "Japanese Bay"
+new.dat$bay[change] <- "Japanese"
+
+change <- new.dat$bay == "Kaiugnak Bay"
+new.dat$bay[change] <- "Kaiugnak"
+
+change <- new.dat$bay == "Rodmans Reach"
+new.dat$bay[change] <- "Rodman Reach"
+
+# and drop Kujulik as this is only sampled one year
+new.dat <- new.dat %>%
+  dplyr::filter(bay != "Kujulik")
+
+# and fix Agripina with trailing space
+change <- new.dat$bay == "Agripina "
+new.dat$bay[change] <- "Agripina"
+
+# and fix Pt Wrangell
+change <- new.dat$bay == "Pt Wrangell"
+new.dat$bay[change] <- "Port Wrangell"
+
+# and check
+unique(new.dat$bay)
+
+# now site
+check.site <- new.dat %>%
+  dplyr::group_by(bay, site) %>%
+  dplyr::summarise(n = n())
+
+View(check.site)
+
+# change Mitro to Mit
+change <- new.dat$site == "Mitro-1"
+new.dat$site[change] <- "Mit-1"
+
+change <- new.dat$site == "Mitro-2"
+new.dat$site[change] <- "Mit-2"
+
+change <- new.dat$site == "Mitro-3"
+new.dat$site[change] <- "Mit-3"
+
+change <- new.dat$site == "Mitro-4"
+new.dat$site[change] <- "Mit-4"
+
+change <- new.dat$site == "Mitro-5"
+new.dat$site[change] <- "Mit-5"
+
+change <- new.dat$site == "Mitro-6"
+new.dat$site[change] <- "Mit-6"
+
+# change Rod to RR
+change <- new.dat$site == "Rod-1"
+new.dat$site[change] <- "RR-1"
+
+change <- new.dat$site == "Rod-2"
+new.dat$site[change] <- "RR-2"
+
+change <- new.dat$site == "Rod-3"
+new.dat$site[change] <- "RR-3"
+
+change <- new.dat$site == "Rod-4"
+new.dat$site[change] <- "RR-4"
+
+change <- new.dat$site == "Rod-5"
+new.dat$site[change] <- "RR-5"
+
+change <- new.dat$site == "Rod-6"
+new.dat$site[change] <- "RR-6"
+
+# Middle cove
+change <- new.dat$site == "Middle cove"
+new.dat$site[change] <- "Middle Cove"
+
+# check again
+check.site <- new.dat %>%
+  dplyr::group_by(bay, site) %>%
+  dplyr::summarise(n = n())
+
+View(check.site)
+
+
 # and save
 write.csv(new.dat, "./data/seine_temperatures_2006_2021.csv", row.names = F)
