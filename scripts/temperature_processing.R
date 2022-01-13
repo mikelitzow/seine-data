3408275de4fedd36da03cef61fe1d4ae4a414095
mikelitzow <mike.litzow@noaa.gov>
2022-01-13 11:37:22 -0900

    Updated seine temperatures through 2021

--- scripts/temperature_processing.R
+++ scripts/temperature_processing.R
@@ -68,3 +68,62 @@ temp.dat$year <- as.numeric(temp.dat$year)
 temp.dat <- na.omit(temp.dat)
 
 write.csv(temp.dat, "./data/seine_temperatures.csv")
+
+## upload with 2021 data
+temp.dat <- read.csv("./data/seine_temperatures.csv", row.names = 1)
+
+# check year - bay sample size
+check <- temp.dat %>%
+  group_by(bay, year) %>%
+  summarise(n = n())
+
+View(check)  
+
+# looks like we're short on Anton Larson and Cook Bay temps in 2020
+# load comparison data
+comp.dat <- read.csv("./data/Kodiak gadid CPUE 2006-2021.csv")
+
+head(comp.dat)
+
+# manipulate
+names(comp.dat)[1] <- "year"
+
+comp.dat$Date <- chron::dates(as.character(comp.dat$Date))
+
+comp.dat$julian <- lubridate::yday(comp.dat$Date)
+
+comp.dat <- comp.dat %>%
+  select(year, julian, Site.Name, Region, Temperature)
+
+names(comp.dat)[3:4] <- c("site", "bay")
+names(comp.dat)[5] <- "temperature"
+
+comp.check <- na.omit(comp.dat) %>%
+  group_by(bay, year) %>%
+  summarise(n = n())
+
+View(comp.check)
+
+# there are more Cook Bay in 2020 that are available
+# remove 2020 Cook Bay from temp.dat;
+# add in 2020 Cook and 2021 Cook's/Anton's
+
+# remove Cook 2020 from existing data
+drop <- temp.dat$year == 2020 & temp.dat$bay == "Cook Bay"
+temp.dat <- temp.dat[!drop,]
+
+# filter comp.dat to include only Cook 2020 and Cook/Anton 2021
+add.dat <- comp.dat %>%
+  filter(year %in% 2020:2021, bay %in% c("Anton Larson Bay", "Cook Bay"))
+
+drop <- add.dat$year == 2020 & add.dat$bay == "Anton Larson Bay"
+add.dat <- add.dat[-drop,]
+View(add.dat)
+
+# and now combine the two data.frames into an updated time series
+names(temp.dat); names(add.dat)
+
+new.dat <- rbind(temp.dat, add.dat)
+
+# and save
+write.csv(new.dat, "./data/seine_temperatures_2006_2021.csv", row.names = F)
