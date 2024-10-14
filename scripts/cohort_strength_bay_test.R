b78eaaeb993dbc52f74e11f907a4c855c4d88292
mikelitzow <mike.litzow@noaa.gov>
2024-10-14 06:24:52 -0800

    Added full set of bays model for comparison

--- scripts/cohort_strength_bay_test.R
+++ scripts/cohort_strength_bay_test.R
@@ -180,13 +180,215 @@ ggplot(plot.cod_test, aes(year_fac, estimate__)) +
   geom_col(color = "black", fill = "grey") +
   geom_errorbar(aes(ymin=lower__, ymax=upper__), width=0.3, size=0.5) +
   ylab("Age-0 cod / set") +
-  scale_y_continuous(breaks=c(1,5,10,50,100,200,300), minor_breaks = NULL) +
+  scale_y_continuous(breaks=c(1,5,10,50,100,200,500,1000,1500), minor_breaks = NULL) +
   coord_trans(y = "pseudo_log") +
   theme(axis.title.x = element_blank())
 
-ggsave("./figs/seine_cod_age0_abundance_estimates_test.png", width = 6, height = 4, units = 'in')
+ggsave("./figs/seine_cod_age0_abundance_estimates_Anton_Cook.png", width = 6, height = 4, units = 'in')
 
 # round, rename columns, and save
 plot.cod_test[,2:5] <- round(plot.cod_test[,2:5], 2)
 names(plot.cod_test) <- c("year", "cod_per_set", "cod_se", "cod_95percent_LCI", "cod_95percent_UCI")
-write.csv(plot.cod_test, "./output/seine_cod_age0_abundance_estimates_test.csv", row.names = F)
+write.csv(plot.cod_test, "./output/seine_cod_age0_abundance_estimates_Cook_Anton.csv", row.names = F)
+
+# second test - full set of Bays with no site_fac term---------------------
+
+## Define model formula - simpler model without site factor
+time.series_formula <-  bf(cod ~ year_fac + s(julian, k = 4) + (1 | bay_fac),
+                           zi ~ year_fac + s(julian, k = 4) + (1 | bay_fac))
+
+## Set model distributions
+zinb <- zero_inflated_negbinomial(link = "log", link_shape = "log", link_zi = "logit")
+
+## Set priors
+priors_zinb <- c(set_prior("normal(0, 3)", class = "b"),
+                 set_prior("normal(0, 3)", class = "Intercept"),
+                 set_prior("student_t(3, 0, 3)", class = "sd"),
+                 set_prior("student_t(3, 0, 3)", class = "sds"),
+                 set_prior("gamma(0.01, 0.01)", class = "shape"),
+                 set_prior("normal(0, 3)", class = "b", dpar = "zi"),
+                 set_prior("logistic(0, 1)", class = "Intercept", dpar = "zi"),
+                 set_prior("student_t(3, 0, 3)", class = "sd", dpar = "zi"),
+                 set_prior("student_t(3, 0, 3)", class = "sds", dpar = "zi"))
+
+
+## cod fit: zero-inflated --------------------------------------
+
+cod_time.series_zinb_test2 <- brm(time.series_formula,
+                                 data = dat,
+                                 prior = priors_zinb,
+                                 family = zinb,
+                                 cores = 4, chains = 4, iter = 3000,
+                                 save_pars = save_pars(all = TRUE),
+                                 control = list(adapt_delta = 0.999, max_treedepth = 12))
+
+saveRDS(cod_time.series_zinb_test2, file = "output/cod_time.series_zinb_test2.rds")
+
+cod_time.series_zinb_test2 <- readRDS("./output/cod_time.series_zinb_test2.rds")
+check_hmc_diagnostics(cod_time.series_zinb_test2$fit)
+neff_lowest(cod_time.series_zinb_test2$fit)
+rhat_highest(cod_time.series_zinb_test2$fit)
+summary(cod_time.series_zinb_test2)
+bayes_R2(cod_time.series_zinb_test2)
+plot(cod_time.series_zinb_test2$criteria$loo, "k")
+plot(conditional_smooths(cod_time.series_zinb_test2), ask = FALSE)
+y <- dat$cod
+yrep_cod_time.series_zinb_test2  <- fitted(cod_time.series_zinb_test2, scale = "response", summary = FALSE)
+ppc_dens_overlay(y = y, yrep = yrep_cod_time.series_zinb_test2[sample(nrow(yrep_cod_time.series_zinb_test2), 25), ]) +
+  xlim(0, 500) +
+  ggtitle("cod_time.series_zinb_test2")
+pdf("./figs/trace_cod_time.series_zinb_test2.pdf", width = 6, height = 4)
+trace_plot(cod_time.series_zinb_test2$fit)
+dev.off()
+
+## Cod predicted effects ---------------------------------------
+
+## 95% CI
+ce1s_1 <- conditional_effects(cod_time.series_zinb_test2, effect = "year_fac", re_formula = NA,
+                              probs = c(0.025, 0.975))  
+
+plot.cod_test2 <- ce1s_1$year_fac %>%
+  select(year_fac, estimate__, se__, lower__, upper__)
+
+
+ggplot(plot.cod_test2, aes(year_fac, estimate__)) +
+  geom_col(color = "black", fill = "grey") +
+  geom_errorbar(aes(ymin=lower__, ymax=upper__), width=0.3, size=0.5) +
+  ylab("Age-0 cod / set") +
+  scale_y_continuous(breaks=c(1,5,10,50,100,200,300), minor_breaks = NULL) +
+  coord_trans(y = "pseudo_log") +
+  theme(axis.title.x = element_blank())
+
+ggsave("./figs/seine_cod_age0_abundance_estimates_all_bays.png", width = 6, height = 4, units = 'in')
+
+# round, rename columns, and save
+plot.cod_test2[,2:5] <- round(plot.cod_test2[,2:5], 2)
+names(plot.cod_test2) <- c("year", "cod_per_set", "cod_se", "cod_95percent_LCI", "cod_95percent_UCI")
+write.csv(plot.cod_test2, "./output/seine_cod_age0_abundance_estimates_all_bays.csv", row.names = F)
+
+
+## compare the two models ------
+test2 <- read.csv("./output/seine_cod_age0_abundance_estimates_all_bays.csv") %>%
+  rename(all = cod_per_set,
+         all_LCI = cod_95percent_LCI,
+         all_UCI = cod_95percent_UCI,
+         all_se = cod_se) 
+
+test1 <- read.csv("./output/seine_cod_age0_abundance_estimates_Cook_Anton.csv") %>%
+  rename(ant_cook = cod_per_set,
+         ant_cook_LCI = cod_95percent_LCI,
+         ant_cook_UCI = cod_95percent_UCI,
+         ant_cook_se = cod_se) 
+
+plot_both <- left_join(test1, test2)
+
+
+ggplot(plot_both, aes(ant_cook, all)) +
+  geom_point() +
+  ylab("All bays cod / set") +
+  xlab("Anton's Cook cod / set") +
+  geom_abline(slope = 1) +
+  geom_smooth(method = "gam", se = F)
+
+ggplot(plot_both, aes(ant_cook_se, all_se)) +
+  geom_point() +
+  ylab("All bays SE") +
+  xlab("Anton's Cook SE") +
+  geom_abline(slope = 1) +
+  geom_smooth(method = "gam", se = F)
+
+test1 <- read.csv("./output/seine_cod_age0_abundance_estimates_Cook_Anton.csv") %>%
+  mutate(group = "All")
+
+test2 <- read.csv("./output/seine_cod_age0_abundance_estimates_all_bays.csv") %>%
+  mutate(group = "Anton_Cook")
+
+plot_both <- rbind(test1, test2)
+
+ggplot(plot_both, aes(ant_cook, all)) +
+  geom_point() +
+  ylab("All bays cod / set") +
+  xlab("Anton's Cook cod / set") +
+  geom_abline(slope = 1) +
+  geom_smooth(method = "gam", se = F)
+
+
+
+# third test - reduced set of Bays with no site_fac term---------------------
+
+dat3 <- dat %>%
+  filter(!bay_fac %in% c("Agripina", "Fox", "Mitrofania", "Port Wrangell", "Rodman Reach", "Ugak"))
+
+## Define model formula - simpler model without site factor
+time.series_formula <-  bf(cod ~ year_fac + s(julian, k = 4) + (1 | bay_fac),
+                           zi ~ year_fac + s(julian, k = 4) + (1 | bay_fac))
+
+## Set model distributions
+zinb <- zero_inflated_negbinomial(link = "log", link_shape = "log", link_zi = "logit")
+
+## Set priors
+priors_zinb <- c(set_prior("normal(0, 3)", class = "b"),
+                 set_prior("normal(0, 3)", class = "Intercept"),
+                 set_prior("student_t(3, 0, 3)", class = "sd"),
+                 set_prior("student_t(3, 0, 3)", class = "sds"),
+                 set_prior("gamma(0.01, 0.01)", class = "shape"),
+                 set_prior("normal(0, 3)", class = "b", dpar = "zi"),
+                 set_prior("logistic(0, 1)", class = "Intercept", dpar = "zi"),
+                 set_prior("student_t(3, 0, 3)", class = "sd", dpar = "zi"),
+                 set_prior("student_t(3, 0, 3)", class = "sds", dpar = "zi"))
+
+
+## cod fit: zero-inflated --------------------------------------
+
+cod_time.series_zinb_test3 <- brm(time.series_formula,
+                                  data = dat3,
+                                  prior = priors_zinb,
+                                  family = zinb,
+                                  cores = 4, chains = 4, iter = 3000,
+                                  save_pars = save_pars(all = TRUE),
+                                  control = list(adapt_delta = 0.999, max_treedepth = 12))
+
+saveRDS(cod_time.series_zinb_test3, file = "output/cod_time.series_zinb_test3.rds")
+
+cod_time.series_zinb_test3 <- readRDS("./output/cod_time.series_zinb_test3.rds")
+check_hmc_diagnostics(cod_time.series_zinb_test3$fit)
+neff_lowest(cod_time.series_zinb_test3$fit)
+rhat_highest(cod_time.series_zinb_test3$fit)
+summary(cod_time.series_zinb_test3)
+bayes_R2(cod_time.series_zinb_test3)
+plot(cod_time.series_zinb_test3$criteria$loo, "k")
+plot(conditional_smooths(cod_time.series_zinb_test3), ask = FALSE)
+y <- dat$cod
+yrep_cod_time.series_zinb_test3  <- fitted(cod_time.series_zinb_test3, scale = "response", summary = FALSE)
+ppc_dens_overlay(y = y, yrep = yrep_cod_time.series_zinb_test3[sample(nrow(yrep_cod_time.series_zinb_test3), 25), ]) +
+  xlim(0, 500) +
+  ggtitle("cod_time.series_zinb_test3")
+pdf("./figs/trace_cod_time.series_zinb_test3.pdf", width = 6, height = 4)
+trace_plot(cod_time.series_zinb_test3$fit)
+dev.off()
+
+## Cod predicted effects ---------------------------------------
+
+## 95% CI
+ce1s_1 <- conditional_effects(cod_time.series_zinb_test2, effect = "year_fac", re_formula = NA,
+                              probs = c(0.025, 0.975))  
+
+plot.cod_test2 <- ce1s_1$year_fac %>%
+  select(year_fac, estimate__, se__, lower__, upper__)
+
+
+ggplot(plot.cod_test2, aes(year_fac, estimate__)) +
+  geom_col(color = "black", fill = "grey") +
+  geom_errorbar(aes(ymin=lower__, ymax=upper__), width=0.3, size=0.5) +
+  ylab("Age-0 cod / set") +
+  scale_y_continuous(breaks=c(1,5,10,50,100,200,300), minor_breaks = NULL) +
+  coord_trans(y = "pseudo_log") +
+  theme(axis.title.x = element_blank())
+
+ggsave("./figs/seine_cod_age0_abundance_estimates_all_bays.png", width = 6, height = 4, units = 'in')
+
+# round, rename columns, and save
+plot.cod_test2[,2:5] <- round(plot.cod_test2[,2:5], 2)
+names(plot.cod_test2) <- c("year", "cod_per_set", "cod_se", "cod_95percent_LCI", "cod_95percent_UCI")
+write.csv(plot.cod_test2, "./output/seine_cod_age0_abundance_estimates_all_bays.csv", row.names = F)
+
